541653d5804fb4891a7523a129bcf6b8150b6d14
JongHyeok <a01024912597@gmail.com>
2026-07-27 17:52:25 +0900

    헬로 수정

--- hello.py
+++ hello.py
@@ -3,9 +3,7 @@ print("hello")
 print("hello2")
 print("hello3")
 print("hello4")
-<<<<<<< HEAD
+
 print("hello4")
 print("hello4")
-=======
-print("hello5")
->>>>>>> 02b79933e8126eff709b800e891cf39f7fbd662a
+
