7fc9e6947ec2f15efcdf34b90843a2e8f9a924ed
JongHyeok <a01024912597@gmail.com>
2026-07-28 10:14:58 +0900

    2123

--- hello.py
+++ hello.py
@@ -2,12 +2,4 @@ print("안녕하세요")
 print("hello")
 print("hello2")
 print("hello3")
-print("hello4")
-
-print("hello4")
-print("hello4")
-
-print("안녕하세요")
-print("hello")
-print("hello2")
-print("hello3")
+print("감사합니다")
